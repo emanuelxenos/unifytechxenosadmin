65df88a4bc4e5335ef6f78642a4bd888ebf624d7
emanuelxenos <manuenbs@gmail.com>
2026-04-29 12:34:54 -0300

    chore: cleanup test files and temporary assets

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    // Basic test to ensure app can be built
-    expect(true, isTrue);
-  });
-}
